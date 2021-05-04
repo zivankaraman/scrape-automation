3d84cbd9a64027065e3ab0786d983d2044cf5ce3
François JOLY <francois@gokam.fr>
2021-05-04 21:45:04 +0100

    Update nifty50_scraping.R

--- nifty50_scraping.R
+++ nifty50_scraping.R
@@ -1,12 +1,10 @@
 #load libs
-
 library(tidyverse)
 library(rvest)
-library(janitor)
 
 #nse top gainers
 
-url <- 'https://www.moneycontrol.com/stocks/marketstats/nsegainer/index.php'
+url <- 'https://www.rforseo.com/sitemap.xml'
 
 # extract html 
 
@@ -14,32 +12,11 @@ url_html <- read_html(url)
 
 #table extraction
 
-url_tables <- url_html %>% html_table(fill = TRUE)
+nbr_url <- url_html %>% 
+  html_nodes("loc")  %>%
+  length()
 
-#extract relevant table
-
-top_gainers <- url_tables[[2]]
-
-#extract relevant columns
-
-top_gainers %>%
-  select(1:7) -> top_gainers
-
-top_gainers %>% 
-  clean_names() -> top_gainers
-
-top_gainers %>%
-  filter(!is.na(low)) -> top_gainers
-
-top_gainers %>%
-  separate(company_name,
-           into = 'company_name',
-           sep = '\t') -> top_gainers
-           
-           
- write_csv(top_gainers,paste0('data/',Sys.Date(),'_top_gainers','.csv'))    
- 
+row <- data.frame(Sys.Date(), nbr_url)
 
 
-
-       
+write_csv(row,paste0('data/xml_url_count.csv'),append = T)    
