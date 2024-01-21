88e9b2fe17fad463cedccc42e3d09c1188c17384
zivankaraman <zivan.karaman@gmail.com>
2024-01-21 12:04:42 +0100

    my version of script

--- sitemap_scraping.R
+++ sitemap_scraping.R
@@ -1,22 +1,6 @@
-#load libs
-library(tidyverse)
-library(rvest)
+# load required libraries
+library(jsonlite)
 
-#nse top gainers
+row <- mtcars
 
-url <- 'https://www.rforseo.com/sitemap.xml'
-
-# extract html 
-
-url_html <- read_html(url)
-
-#table extraction
-
-nbr_url <- url_html %>% 
-  html_nodes("loc")  %>%
-  length()
-
-row <- data.frame(Sys.Date(), nbr_url)
-
-
-write_csv(row,paste0('data/xml_url_count.csv'),append = T)    
+write.csv(row, paste0('data/xml_url_count.csv'), append = TRUE)
